317dbb709da9f93dd3bcc111afc85c143b23733f
unknown <leanhtuan131104@gmail.com>
2025-10-28 10:29:19 +0700

    service and dependency injection

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Get()
-  getAllUsers(): string {
-    return 'This action returns all users';
+  getAllUsers(): string[] {
+    return [
+      this.authService.authenticateUser(),
+      this.usersService.getAllUsers(),
+    ];
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { DatabaseService } from '../db/db.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService, DatabaseService],
 })
 export class UsersModule {}
